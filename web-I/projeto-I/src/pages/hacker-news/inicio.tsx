9c711a417ba6d3d85784bda212e0c1d39701b8f5
Bruno Martins <bgmartins@ucs.br>
2024-08-26 19:37:30 -0300

    feat: :sparkles: adição de icones no cabeçalho

--- web-I/projeto-I/src/pages/hacker-news/inicio.tsx
+++ web-I/projeto-I/src/pages/hacker-news/inicio.tsx
@@ -1,5 +1,7 @@
 import { Cabecalho } from "@/entities/cabecalho";
 import { NavBar, NavBarLink } from "@/entities/navbar";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function HackerNewsPage() {
   return (
@@ -7,12 +9,18 @@ export function HackerNewsPage() {
       <div className="bg-header-gradient w-100">
         <Cabecalho />
       </div>
-      <NavBar>
-        <NavBarLink href="#home">Home</NavBarLink>
-        <NavBarLink href="#cyber-attacks">Cyber Attacks</NavBarLink>
-        <NavBarLink href="#vulnerabilities">Vulnerabilities</NavBarLink>
-        <NavBarLink href="#store">Store</NavBarLink>
-        <NavBarLink href="#contact">Contact</NavBarLink>
+      <NavBar className="flex">
+        <div className="flex items-center mr-auto">
+          <NavBarLink href="#home">Home</NavBarLink>
+          <NavBarLink href="#cyber-attacks">Cyber Attacks</NavBarLink>
+          <NavBarLink href="#vulnerabilities">Vulnerabilities</NavBarLink>
+          <NavBarLink href="#store">Store</NavBarLink>
+          <NavBarLink href="#contact">Contact</NavBarLink>
+        </div>
+        <div className="flex items-center">
+          <FontAwesomeIcon icon={faSearch} className="text-muted" />
+          <FontAwesomeIcon icon={faBars} className="text-muted" />
+        </div>
       </NavBar>
     </div>
   );
