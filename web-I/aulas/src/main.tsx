affac604fe5f3b9d4c3b908a59728188f9359326
bgmartins <bgmartin@ucs.br>
2024-09-02 21:32:09 -0300

    feat: :fire: Tirando proj1 desta pasta

--- web-I/aulas/src/main.tsx
+++ web-I/aulas/src/main.tsx
@@ -6,25 +6,15 @@ import "./index.css";
 
 import MyPodPage from "./pages/aula/inicio.tsx";
 import { IpodPage } from "./pages/aula/ipod.tsx";
-import { NoticiaDetailPage } from "./pages/hacker-news/[slug]/page.tsx";
-import { HackerNewsPage } from "./pages/hacker-news/page.tsx";
 
 const router = createBrowserRouter([
   {
-    path: "/my-pod",
+    path: "/",
     element: <MyPodPage />,
   },
   {
     path: "/my-pod/ipod",
     element: <IpodPage />,
-  },
-  {
-    path: "/",
-    element: <HackerNewsPage />,
-  },
-  {
-    path: "/noticia/:slug",
-    element: <NoticiaDetailPage />,
   },
 ]);
 createRoot(document.getElementById("root")!).render(
