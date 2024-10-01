75edc37ea2030db348bd50847740106aa68a5d74
Bruno Gonçalves Martins <108153115+Relampago-Martins@users.noreply.github.com>
2024-10-01 10:51:08 -0300

    Correção do somvet.asm
    Correção do somvet.asm para que os partametros sejam passados por registradores.

--- arquitetura_computadores/TDE-A/somvet.asm
+++ arquitetura_computadores/TDE-A/somvet.asm
@@ -2,25 +2,15 @@
 .stack 100H  
 .data 
     TAM_VETOR EQU 5             	; define uma constante de valor 5
-    vetor dw 1, 2, 3, 4, 5      		; define um vetor de 5 posições
+    vetor dw 1, 2, 3, 4, 5      		; define um vetor de 5 posiÃ§Ãµes
 .code 
 
 SOMA_VETOR16 proc
-    push BX   
-    push CX 
-    
-    ; Soma os valores do vetor
-    xor AX, AX                 	
-    mov BX, offset vetor         		; BX recebe a posição inicial do vetor
-    mov CX, TAM_VETOR           	; CX = número de elementos do vetor
-
+    xor AX, AX    
  ACUMULA:          
-    add AX,[BX]			;BX contém o valor da primeira posição  
-    add BX, 2 				;pula 2 posições pois numero tem 2 bytes
-    loop ACUMULA 			;decrementa de CX=5 até CX=0         
-                     	
-    pop CX    
-    pop BX    
+    add AX,[BX]			;BX contÃ©m o valor da primeira posiÃ§Ã£o  
+    add BX, 2 				;pula 2 posiÃ§Ãµes pois numero tem 2 bytes
+    loop ACUMULA 			;decrementa de CX=5 atÃ© CX=0         
 ret
 endp    
 
@@ -30,23 +20,23 @@ PRINT16 proc
     push CX
     push DX 
     
-    xor DX, DX                   		; Zera DX para divisão
-    mov BX, 10                   		; Divisor para separação de dígitos
-    xor CX, CX                   		; Zera o contador de dígitos
+    xor DX, DX                   		; Zera DX para divisÃ£o
+    mov BX, 10                   		; Divisor para separaÃ§Ã£o de dÃ­gitos
+    xor CX, CX                   		; Zera o contador de dÃ­gitos
 
     LACO_DIV:
-    xor DX, DX   			;zerar DX pois o dividendo é DXAX
-    div BX       				;divisão para separar o dígito em DX
+    xor DX, DX   			;zerar DX pois o dividendo Ã© DXAX
+    div BX       				;divisÃ£o para separar o dÃ­gito em DX
     push DX     
-    inc CX       				;incrementa o contador para chegar até CX=5
+    inc CX       				;incrementa o contador para chegar atÃ© CX=5
     cmp AX, 0    		
     jnz LACO_DIV 			;enquanto AX =! 0 salte para LACO_DIV
            
     LACO_ESCDIG:
-    pop DX       				;desempilha o dígito    
-    add DL, '0'  				;converter o dígito para ASCII
+    pop DX       				;desempilha o dÃ­gito    
+    add DL, '0'  				;converter o dÃ­gito para ASCII
     call ESC_CHAR               
-    loop LACO_ESCDIG    		;decrementa CX=5 até CX=0
+    loop LACO_ESCDIG    		;decrementa CX=5 atÃ© CX=0
           
     pop DX  
     pop CX    
@@ -68,6 +58,9 @@ INICIO:
     mov AX, @DATA
     mov DS, AX 
 
+    ;define parametros
+    mov BX, offset vetor            ; BX recebe a posiÃ§Ã£o inicial do vetor
+    mov CX, TAM_VETOR           	; CX = nÃºmero de elementos do vetor
     call SOMA_VETOR16  
     ; Multiplica o resultado por 2
     shl AX, 1 ; Chama a rotina para somar, multiplicar e exibir o resultado
